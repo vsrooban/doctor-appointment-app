ab8f4aeb1e30636401639e9d5d5387f97e9e58b4
vasanth <vasanthdevi552003@gmail.com>
2025-03-03 19:03:09 +0530

    doctors details added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:doctor_appointment_app/screen/home/doctor_details_screen.dart';
 import 'package:doctor_appointment_app/screen/login/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -23,7 +24,7 @@ class _MyAppState extends State<MyApp> {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: WelcomeScreen(),
+      home: DoctorDetailsScreen(),
     );
   }
 }
--- lib/widget/doctor_details_card_widget.dart
+++ lib/widget/doctor_details_card_widget.dart
@@ -1,90 +1,62 @@
 import 'package:doctor_appointment_app/util/custom_theme.dart';
-// import 'package:doctor_appointment_app/widget/Doctor_card.dart';
 import 'package:flutter/material.dart';
 
-class DoctorDetailsCardWidget extends StatefulWidget {
-  const DoctorDetailsCardWidget({super.key,required this.doctors,});
+class DoctorDetailsCardWidget extends StatelessWidget {
+  DoctorDetailsCardWidget({super.key});
 
- final List<Map<String, String>> doctors;
-  @override
-  State<DoctorDetailsCardWidget> createState() => _DoctorDetailsCardWidgetState();
-}
+  final Map<String, String> doctor = {
+    "name": "Dr. David Patel",
+    "specialty": "Cardiologist",
+    "hospital": "Cardiology Center, USA",
+    "image": "assets/images/doctor_4.png",
+  };
 
-class _DoctorDetailsCardWidgetState extends State<DoctorDetailsCardWidget> {
-   final List<Map<String, String>> doctors = [
-    {
-      "name": "Dr. David Patel",
-      "specialty": "Cardiologist",
-      "hospital": "Cardiology Center, USA",
-      "image": "assets/images/doctor_4.png",
-    },
-   ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: ListView.builder(
-      itemCount: widget.doctors.length,
-      itemBuilder: (context, index) {
-        final doctor = widget.doctors[index];
-        return Card(
-          elevation: 5,
-          color: Colors.white,
-          margin: const EdgeInsets.symmetric(horizontal: .9, vertical: 8.0),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(12),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  width: 95,
-                  height: 120,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.grey[300],
-                    image: DecorationImage(
-                      image: AssetImage(doctor['image']!),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
+    return Card(
+      elevation: 5,
+      color: Colors.white,
+      margin: const EdgeInsets.symmetric(horizontal: .9, vertical: 8.0),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 95,
+              height: 120,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.grey[300],
+                image: DecorationImage(
+                  image: AssetImage(doctor['image']!),
+                  fit: BoxFit.cover,
                 ),
-                const SizedBox(width: 12),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+              ),
+            ),
+            const SizedBox(width: 12),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(doctor['name']!, style: AppTypography.bodyLG),
+                  const Divider(thickness: .3, color: Colors.grey),
+                  Text(doctor['specialty']!, style: AppTypography.bodySBold),
+                  Row(
                     children: [
-                      Row(
-                        mainAxisAlignment:
-                            MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(doctor['name']!, style: AppTypography.bodyLG),
-                          
-                        ],
-                      ),
-                      const Divider(thickness: .3, color: Colors.grey),
-                      Text(doctor['specialty']!, style: AppTypography.bodySBold),
-                      Row(
-                        children: [
-                          const Icon(Icons.location_on_outlined, size: 12),
-                          Text(doctor['hospital']!, style: AppTypography.bodySRegular),
-                        ],
-                      ),
-                  
+                      const Icon(Icons.location_on_outlined, size: 12),
+                      Text(doctor['hospital']!, style: AppTypography.bodySRegular),
                     ],
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        );
-      },
-    ),
+          ],
         ),
-      
+      ),
     );
   }
 }
