0bc0fc3bfd58d90fd6ff84b3b86ebe1895818526
vasanth <vasanthdevi552003@gmail.com>
2025-03-03 18:00:54 +0530

    changes moves fine tune

---
+++ lib/screen/home/doctor_details_screen.dart
@@ -0,0 +1,38 @@
+import 'package:doctor_appointment_app/util/custom_theme.dart';
+import 'package:doctor_appointment_app/widget/Doctor_card.dart';
+import 'package:doctor_appointment_app/widget/favourite_button.dart';
+import 'package:flutter/material.dart';
+
+class DoctorDetailsScreen extends StatefulWidget {
+  const DoctorDetailsScreen({super.key});
+
+  @override
+  State<DoctorDetailsScreen> createState() => _DoctorDetailsScreenState();
+}
+
+class _DoctorDetailsScreenState extends State<DoctorDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(onPressed: (){}, icon: Icon(Icons.arrow_back)),
+                Text('Doctor Details',style: AppTypography.h1,),
+                SizedBox(child: FavoriteButton(),height: 20,width: 20,),
+
+              ],
+            ),
+            SizedBox(height: 15,),
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
